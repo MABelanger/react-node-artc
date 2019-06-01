35b3930857105e19deb9c44590db89161e760476
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2019-06-01 17:39:57 -0400

    refactor

--- react/src/Part/DownloadButton.js
+++ react/src/Part/DownloadButton.js
@@ -1,28 +1,11 @@
 import React from 'react';
 
-// https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
-function downloadJson (data, fileName) {
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-
-    let json = JSON.stringify(data);
-    let blob = new Blob([json], {type: "octet/stream"});
-    let url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = fileName;
-    a.click();
-    window.URL.revokeObjectURL(url);
-}
-
-
-const PostButton = ({data, jsonName}) => {
-  console.log('jsonName', jsonName);
+const DownloadButton = ({onDownload}) => {
   return (
     <button onClick={()=>{
-      downloadJson(data, jsonName);
+      onDownload();
     }}>download</button>
   );
 }
 
-export default PostButton;
+export default DownloadButton;
