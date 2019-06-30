34b77372a36aa8262825bc3e03105e9ccba44bf4
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2019-06-30 13:10:10 -0400

    display images

--- react/src/Loa/Images.js
+++ react/src/Loa/Images.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Images(props) {
   let { imagePaths } = props;
+
+  const imgStyle = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'block'
+  };
+
   return (
     imagePaths && imagePaths.slice(0).reverse().map((imagePath)=>{
-      return <img style={{marginLeft: 'auto', marginRight: 'auto', display: 'block'}} src={imagePath} />
+      return <img style={imgStyle} src={imagePath} />
     })
   );
 }
